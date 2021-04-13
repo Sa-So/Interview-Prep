a085b4dcd3fc799d9aae1c26d993ea410f3f629e
Sa-So <60461699+Sa-So@users.noreply.github.com>
2021-04-13 12:00:44 +0530

    Update binary_search.cpp

--- Algorithms-STL/binary_search.cpp
+++ Algorithms-STL/binary_search.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main()
 {
-  int arr[]={4,5,3,2,1};//unsorted array !
+  int arr[]={20,30,40,40,40,60,100};//sorted array !
   int n=sizeof(arr)/sizeof(int);
   
   int key=3;
@@ -15,5 +15,17 @@ int main()
     }
     else{
         cout<<"absent"<<endl;
+    }
+    //lower_bound(s,e,key)
+    int* lb = lower_bound(arr,arr+n,40);//gives address of first bucket >=key
+    cout<<(it-arr)<<endl;
+    
+    int* ub = upper_bound(arr,arr+n,40);//gives address of first bucket >key
+    cout<<(it-arr)<<endl;
+    
+    cout<<"Occ freq = "<<ub-lb<<endl;
+  
+    if(ub-lb==0){
+      cout<<"element not present"<<endl;
     }
 }
