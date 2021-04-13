a486bdc5091fe200394ad1a0f89fb5d70ec3936e
Sa-So <60461699+Sa-So@users.noreply.github.com>
2021-04-13 20:45:20 +0530

    Create cmp.cpp

---
+++ Algorithms-STL/cmp.cpp
@@ -0,0 +1,5 @@
+// lower_bound(s,e,key,comp) also accepts comparator
+// it should be true if first value should come before second value
+// by default it is < 
+// so if u want to include = numbers before the number and then find the number position 
+// write a comparator function returning a<=b
